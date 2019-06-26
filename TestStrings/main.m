8ea15dcc7e2339e9d9320ae9096f96dca110074d
Austin <austin@tag219877.lrc.txstate.edu>
2019-06-26 12:30:38 -0500

    Initial Commit

---
+++ TestStrings/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  TestStrings
+//
+//  Created by Austin on 6/25/19.
+//  Copyright © 2019 Austin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello World!");
+    }
+    
+    NSString *firstName;
+    firstName = @"Jamie";
+    
+    NSLog(@"firstName: %@", firstName);
+    
+    return 0;
+}
